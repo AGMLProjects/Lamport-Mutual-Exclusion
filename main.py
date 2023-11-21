19fcd16959246984c7edb4d207094af8dafce7e4
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2023-11-21 15:53:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,17 @@ import socket
 import threading
 import time
 import pickle
+import random
 
-import socket
-import threading
-import time
-import pickle
-
-# Function for sending messages
 def send_message(destination, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(destination)
         s.sendall(pickle.dumps(message))
 
-# Function for receiving messages
 def receive_message(socket_conn):
     data = socket_conn.recv(1024)
     return pickle.loads(data)
 
-# Implementation of Lamport's Mutual Exclusion Algorithm
 class LamportAlgorithm:
     def __init__(self, process_id, num_processes):
         self.process_id = process_id
@@ -27,20 +20,31 @@ class LamportAlgorithm:
         self.logical_clock = [0] * num_processes
         self.in_critical_section = False
         self.request_queue = []
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('localhost', 8000 + process_id))
+        self.server_socket.listen(5)
+
+    def should_request(self):
+        # Adjust this method based on the desired behavior
+        # For example, you can use a probability factor
+        return random.random() < 0.5  # Simulates a 50% chance of wanting to access the critical section
 
     def request_access(self):
         self.in_critical_section = False
         self.logical_clock[self.process_id] += 1
 
-        for i in range(self.num_processes):
-            if i != self.process_id:
-                message = {'type': 'request', 'clock': self.logical_clock.copy(), 'process_id': self.process_id}
-                send_message(('localhost', 8000 + i), message)
+        if self.should_request():
+            for i in range(self.num_processes):
+                if i != self.process_id:
+                    message = {'type': 'request', 'clock': self.logical_clock.copy(), 'process_id': self.process_id}
+                    send_message(('localhost', 8000 + i), message)
 
-        while len(self.request_queue) < self.num_processes - 1:
-            time.sleep(1)
+            self.request_queue.append(self.process_id)
 
-        self.enter_critical_section()
+            while len(self.request_queue) < self.num_processes - (1 if self.num_processes > 1 else 0):
+                time.sleep(1)
+
+            self.enter_critical_section()
 
     def enter_critical_section(self):
         print(f'Process {self.process_id} enters the critical section.')
@@ -82,34 +86,33 @@ class LamportAlgorithm:
             else:
                 self.in_critical_section = False
 
+        elif message['type'] == 'response':
+            self.logical_clock = [max(local, remote) for local, remote in zip(self.logical_clock, message['clock'])]
+            self.logical_clock[self.process_id] += 1
+            if message['process_id'] in self.request_queue:
+                self.request_queue.remove(message['process_id'])
 
-# Function to manage a process's connection
 def handle_connection(client_socket, lamport):
     while True:
         message = receive_message(client_socket)
         lamport.handle_message(message)
 
-
-# Main function
 def main():
-    num_processes = 10  # Set the number of processes
+    num_processes = 3
     processes = []
 
     for i in range(num_processes):
         lamport_algorithm = LamportAlgorithm(i, num_processes)
-        t = threading.Thread(target=handle_connection, args=((('', 8000 + i)), lamport_algorithm))
+        t = threading.Thread(target=handle_connection, args=(lamport_algorithm.server_socket.accept() + (lamport_algorithm,),))
         t.start()
         processes.append(t)
 
-    # Simulation: The first two processes request access to the critical section
-    time.sleep(2)
-    processes[0].request_access()
-    time.sleep(1)
-    processes[1].request_access()
+    # Simulation: Processes decide whether to request access
+    for lamport_algorithm in [lamport for lamport in processes]:
+        lamport_algorithm.request_access()
 
     for t in processes:
         t.join()
-
 
 if __name__ == "__main__":
     main()
